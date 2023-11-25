a5c9ef1bd3cbae692f7360e9b446b43e758d0990
kos4 <kos4.kks@gmail.com>
2023-11-25 12:38:21 +0500

    Домашнее задание №1.

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,24 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let discriminant = Math.pow(b, 2) - 4 * a * c;
+
+  if (discriminant === 0) {
+    arr.push(-b / (2 * a));
+  } else if (discriminant > 0) {
+    arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
+    arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
+  }
   
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let percentMonth = percent / (100 * 12);
+  let loanBody = amount - contribution;
+  let monthlyPayment = loanBody * (percentMonth + (percentMonth / ((Math.pow(1 + percentMonth, countMonths) - 1))));
+  let totalSum = monthlyPayment * countMonths;
+  totalSum = Number(totalSum.toFixed(2));
+
+  return totalSum;
 }
