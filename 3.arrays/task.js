0927fd147646e4dcb1be961f6dcc1a0df5f24177
kos4 <kos4.kks@gmail.com>
2023-12-06 00:04:49 +0500

    Домашнее задание №3.

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.every((item, index, arr) => item === arr2[index] && arr.length === arr2.length);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let sum = 0;
+
+  return users.filter(user => user.gender === gender).reduce((acc, item, index, arr) => {
+    sum += item.age;
+    if (arr.length === index + 1) {
+      return sum/arr.length;
+    }
+  }, 0);
 }
