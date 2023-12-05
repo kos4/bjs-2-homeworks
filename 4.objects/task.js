fb717b40c2ccc54a3953fa38adf9f3694ac89367
kos4 <kos4.kks@gmail.com>
2023-12-06 01:15:52 +0500

    Домашнее задание №4.

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.hasOwnProperty('marks')) {
+    this.marks = [...this.marks, ...marks];
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (!this.hasOwnProperty('marks') || this.marks.length === 0) return 0;
+
+  let sum = 0;
+
+  return this.marks.reduce((acc, item, index, arr) => {
+    sum += item;
+
+    if (arr.length === index + 1) {
+      return sum / arr.length;
+    }}, 0);
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  this.excluded = reason;
+  delete this.marks;
+  delete this.subject;
 }
